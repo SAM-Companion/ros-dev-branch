0e005a7484139f406283fdd04b8ac339bfec9d9f
LeopardCheetah <hawkswithmath@gmail.com>
2025-08-28 17:32:12 -0700

    iuginreop

--- tutorials/ros2_ws/build/py_tutorial_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sam-robot-pi/Documents/ros-dev/tutorials/ros2_ws/install/py_tutorial_pubsub'
